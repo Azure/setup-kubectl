00c14b274c390ac2b52274daf210ac10b055dc48
raghaves <raghaves@microsoft.com>
2020-06-16 17:54:50 +0530

    Review comments fix: run npm install only with our build, npm install only dev for releases

--- __tests__/run.test.ts
+++ __tests__/run.test.ts
@@ -1,4 +1,4 @@
-import {run} from '../src/run'
+import { run } from '../src/run'
 
 describe('This is a placeholder for intial test cases, to be removed', () => {
     test('Dummy test case', async () => {
