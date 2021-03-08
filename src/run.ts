31a74bd1dfb9b320049b03b19ca2ce8b8b5044b6
Sundar Guntnur <sundargs2000@github.com>
2021-03-08 21:40:07 +0530

    Added L0 tests.

--- src/run.ts
+++ src/run.ts
@@ -10,14 +10,14 @@ const kubectlToolName = 'kubectl';
 const stableKubectlVersion = 'v1.15.0';
 const stableVersionUrl = 'https://storage.googleapis.com/kubernetes-release/release/stable.txt';
 
-function getExecutableExtension(): string {
+export function getExecutableExtension(): string {
     if (os.type().match(/^Win/)) {
         return '.exe';
     }
     return '';
 }
 
-function getkubectlDownloadURL(version: string): string {
+export function getkubectlDownloadURL(version: string): string {
     switch (os.type()) {
         case 'Linux':
             return util.format('https://storage.googleapis.com/kubernetes-release/release/%s/bin/linux/amd64/kubectl', version);
@@ -32,7 +32,7 @@ function getkubectlDownloadURL(version: string): string {
     }
 }
 
-async function getStableKubectlVersion(): Promise<string> {
+export async function getStableKubectlVersion(): Promise<string> {
     return toolCache.downloadTool(stableVersionUrl).then((downloadPath) => {
         let version = fs.readFileSync(downloadPath, 'utf8').toString().trim();
         if (!version) {
@@ -46,7 +46,7 @@ async function getStableKubectlVersion(): Promise<string> {
     });
 }
 
-async function downloadKubectl(version: string): Promise<string> {
+export async function downloadKubectl(version: string): Promise<string> {
     let cachedToolpath = toolCache.find(kubectlToolName, version);
     let kubectlDownloadPath = '';
     if (!cachedToolpath) {
