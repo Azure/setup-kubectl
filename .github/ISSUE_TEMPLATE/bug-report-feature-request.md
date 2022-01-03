cbed02841b94341ad24566d6bda385fdf47b4170
Tommy Barnes <thbarnes@microsoft.com>
2022-01-03 14:50:24 -0500

    Did some reorganizing of code in run.ts, moved run.test.ts into /src, and put some helpers into helpers.ts in /src.

--- .github/ISSUE_TEMPLATE/bug-report-feature-request.md
+++ .github/ISSUE_TEMPLATE/bug-report-feature-request.md
@@ -3,7 +3,7 @@ name: Bug Report / Feature Request
 about: Create a report to help us improve
 title: ''
 labels: need-to-triage
-assignees: ''
+assignees: '@aksatlanta'
 
 ---
 
--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,3 @@
-import * as os from 'os';
 import * as path from 'path';
 import * as util from 'util';
 import * as fs from 'fs';
@@ -6,38 +5,23 @@ import * as fs from 'fs';
 import * as toolCache from '@actions/tool-cache';
 import * as core from '@actions/core';
 
+import * as helpers from './helpers';
+
 const kubectlToolName = 'kubectl';
 const stableKubectlVersion = 'v1.15.0';
 const stableVersionUrl = 'https://storage.googleapis.com/kubernetes-release/release/stable.txt';
 
-export function getExecutableExtension(): string {
-    if (os.type().match(/^Win/)) {
-        return '.exe';
+export async function run() {
+    let version = core.getInput('version', { 'required': true });
+    if (version.toLocaleLowerCase() === 'latest') {
+        version = await getStableKubectlVersion();
     }
-    return '';
-}
+    const cachedPath = await downloadKubectl(version);
 
-export function getKubectlArch(): string {
-    let arch = os.arch();
-    if (arch === 'x64') {
-        return 'amd64';
-    }
-    return arch;
-}
+    core.addPath(path.dirname(cachedPath));
 
-export function getkubectlDownloadURL(version: string, arch: string): string {
-    switch (os.type()) {
-        case 'Linux':
-            return util.format('https://storage.googleapis.com/kubernetes-release/release/%s/bin/linux/%s/kubectl', version, arch);
-
-        case 'Darwin':
-            return util.format('https://storage.googleapis.com/kubernetes-release/release/%s/bin/darwin/%s/kubectl', version, arch);
-
-        case 'Windows_NT':
-        default:
-            return util.format('https://storage.googleapis.com/kubernetes-release/release/%s/bin/windows/%s/kubectl.exe', version, arch);
-
-    }
+    console.log(`Kubectl tool version: '${version}' has been cached at ${cachedPath}`);
+    core.setOutput('kubectl-path', cachedPath);
 }
 
 export async function getStableKubectlVersion(): Promise<string> {
@@ -57,10 +41,10 @@ export async function getStableKubectlVersion(): Promise<string> {
 export async function downloadKubectl(version: string): Promise<string> {
     let cachedToolpath = toolCache.find(kubectlToolName, version);
     let kubectlDownloadPath = '';
-    let arch = getKubectlArch();
+    const arch = helpers.getKubectlArch();
     if (!cachedToolpath) {
         try {
-            kubectlDownloadPath = await toolCache.downloadTool(getkubectlDownloadURL(version, arch));
+            kubectlDownloadPath = await toolCache.downloadTool(helpers.getkubectlDownloadURL(version, arch));
         } catch (exception) {
             if (exception instanceof toolCache.HTTPError && exception.httpStatusCode === 404) {
                 throw new Error(util.format("Kubectl '%s' for '%s' arch not found.", version, arch));
@@ -69,25 +53,12 @@ export async function downloadKubectl(version: string): Promise<string> {
             }
         }
 
-        cachedToolpath = await toolCache.cacheFile(kubectlDownloadPath, kubectlToolName + getExecutableExtension(), kubectlToolName, version);
+        cachedToolpath = await toolCache.cacheFile(kubectlDownloadPath, kubectlToolName + helpers.getExecutableExtension(), kubectlToolName, version);
     }
 
-    const kubectlPath = path.join(cachedToolpath, kubectlToolName + getExecutableExtension());
+    const kubectlPath = path.join(cachedToolpath, kubectlToolName + helpers.getExecutableExtension());
     fs.chmodSync(kubectlPath, '777');
     return kubectlPath;
-}
-
-export async function run() {
-    let version = core.getInput('version', { 'required': true });
-    if (version.toLocaleLowerCase() === 'latest') {
-        version = await getStableKubectlVersion();
-    }
-    let cachedPath = await downloadKubectl(version);
-
-    core.addPath(path.dirname(cachedPath));
-            
-    console.log(`Kubectl tool version: '${version}' has been cached at ${cachedPath}`);
-    core.setOutput('kubectl-path', cachedPath);
 }
 
 run().catch(core.setFailed);
